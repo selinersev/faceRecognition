c38842cd0914826fbd457a62c8e5b70fbf58b8f6
Selin Ersev <sersev96@gmail.com>
2018-12-26 11:56:48 +0300

    first commit

---
+++ detection.py
@@ -0,0 +1,37 @@
+import face_recognition
+import cv2
+
+def main():
+    path = "/Users/selinersev/Desktop/SeniorProject/portrait_dataset/"
+
+    for i in range(1,11):
+        img_path = str(path)+str(i)+'.png'
+        frame = cv2.imread(img_path)
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        for (top, right, bottom, left) in face_locations:
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Display the resulting image
+        cv2.imshow(str(i), frame)
+        cv2.waitKey(10)
+
+    cv2.waitKey(0)
+
+
+def print_result(filename, location):
+    top, right, bottom, left = location
+    print("{},{},{},{},{}".format(filename, top, right, bottom, left))
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ recognition.py
@@ -0,0 +1,77 @@
+
+import face_recognition
+from scipy.spatial import distance
+import os, shutil
+
+import cv2
+
+def main():
+    path = "/Users/selinersev/Desktop/orl_faces/"
+    test = []
+    train = []
+    train_person_name = []
+    test_person_name = []
+    train_person_filename = []
+    test_person_filename = []
+    fail_count = 0
+    failure_path = "/Users/selinersev/Desktop/featureExtractionFailure/"
+
+    for i in range(1,41):
+        for j in range(1,6):
+            img_path = str(path)+'s'+str(i)+'/'+str(j)+'.pgm'
+            img = face_recognition.load_image_file(img_path)
+            train_faces_encodings = face_recognition.face_encodings(img)
+            if len(train_faces_encodings) == 0:
+                shutil.copyfile(img_path, failure_path+str(i)+'.'+str(j)+'.pgm')
+                #os.rename(img_path, failure_path+str(i)+'.'+str(j)+'.pgm')
+                print ('excluded train data --> s'+str(i)+" "+str(j))
+
+            else:
+                train.append(train_faces_encodings[0])
+                train_person_name.append('s' + str(i))
+                train_person_filename.append(str(j)+'.pgm')
+
+    for i in range(1,41):
+        for j in range(6,11):
+            img_path = str(path)+'s'+str(i)+'/'+str(j)+'.pgm'
+            img = face_recognition.load_image_file(img_path)
+            test_faces_encodings = face_recognition.face_encodings(img)
+            if len(test_faces_encodings) == 0:
+                print ('excluded test data --> s'+str(i)+" "+str(j))
+            else:
+                test.append(test_faces_encodings[0])
+                test_person_name.append('s' + str(i))
+                test_person_filename.append(str(j)+'.pgm')
+
+    success_count = 0
+    test_index = 0
+    for testVector in test:
+        temp_array = []
+        for trainVector in train:
+            dst = distance.euclidean(trainVector, testVector)
+            temp_array.append(dst)
+        min_index = temp_array.index(min(temp_array))
+        min_person = train_person_name[min_index]
+        test_per_name = test_person_name[test_index]
+        min_person_file = train_person_filename[min_index]
+        test_per_name_file = test_person_filename[test_index]
+        print(min_person)
+        print('-----')
+        print(test_per_name)
+        print (min_person_file+' === '+test_per_name_file)
+        print ('*-*-*-*-*-*-*-*-*-*-*-')
+        if min_person == test_per_name:
+            success_count += 1
+        else:
+            fail_count += 1
+            print ('Fail!!!! ' + min_person)
+        test_index += 1
+
+    print ('successCount:'+str(success_count))
+    print ('failCount:' + str(fail_count))
+    rate = success_count / (success_count + fail_count)
+    print('rate:'+str(rate))
+
+
+if __name__ == "__main__":
+    main()
